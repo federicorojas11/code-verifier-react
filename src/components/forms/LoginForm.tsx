685074877a33b3e2f3561c0b0ba25f4119d7d005
Federico Rojas <federico.rojas.121@gmail.com>
2023-05-01 04:04:51 -0300

    login form created - added login request

---
+++ src/components/forms/LoginForm.tsx
@@ -0,0 +1,97 @@
+import React from "react";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
+import { login } from "../../services/authService";
+import { AxiosResponse } from "axios";
+
+// Define Schema of validation with Yup
+const loginSchema = Yup.object().shape({
+  email: Yup.string()
+    .email("Invalid Email Format")
+    .required("Email is required"),
+  password: Yup.string().required("Password is required"),
+});
+
+// Login Component
+const LoginForm = () => {
+  // Define inital auth credentials
+  const initialCredentials = {
+    email: "",
+    password: "",
+  };
+
+  return (
+    <div>
+      <h4>Login Form</h4>
+      <Formik
+        initialValues={initialCredentials}
+        validationSchema={loginSchema}
+        onSubmit={async (values) => {
+          // await new Promise((response) => setTimeout(() => response, 1000));
+          // alert(JSON.stringify(values, null, 2));
+          // console.table(values);
+
+          login(values.email, values.password)
+            .then((response: AxiosResponse) => {
+              if (response.status === 200) {
+                if (response.data.token) {
+                  sessionStorage.setItem("sessionJWToken", response.data.token);
+                } else {
+                  throw new Error("Error generating auth token");
+                }
+              } else {
+                throw new Error("Invalid credentials");
+              }
+            })
+            .catch((error) => {
+              console.log(`[LOGIN ERROR] Something where wrong: ${error}`);
+            });
+        }}
+      >
+        {({
+          values,
+          touched,
+          errors,
+          isSubmitting,
+          handleChange,
+          handleBlur,
+        }) => (
+          <Form>
+            {/* Email Field */}
+            <label htmlFor="email">Email</label>
+            <Field
+              id="email"
+              type="email"
+              name="email"
+              placeholder="example@email.com"
+            />
+            {/* Email Errors */}
+            {errors.email && touched.email && (
+              <ErrorMessage name="email" component="div"></ErrorMessage>
+            )}
+
+            {/* Passowrd Field */}
+            <label htmlFor="password">Passowrd</label>
+            <Field
+              id="password"
+              type="password"
+              name="password"
+              placeholder="********"
+            />
+            {/* Password Errors */}
+            {errors.password && touched.password && (
+              <ErrorMessage name="password" component="div"></ErrorMessage>
+            )}
+
+            {/* Submit */}
+            <button type="submit">Login</button>
+
+            {isSubmitting ? <p>Checking credentials...</p> : null}
+          </Form>
+        )}
+      </Formik>
+    </div>
+  );
+};
+
+export default LoginForm;
---
+++ src/services/authService.ts
@@ -0,0 +1,45 @@
+import axios from "../utils/config/axios.config";
+/**
+ * Login method
+ * @param {string} email
+ * @param {string} password
+ * @returns
+ */
+export const login = (email: string, password: string) => {
+  // declare body to POST
+  let body = {
+    email: email,
+    password: password,
+  };
+
+  // send POST login request endpoint
+  // http://localhost:8000/api/auth/login
+  return axios.post("/auth/login", body);
+};
+
+/**
+ * Register method
+ * @param {string} email
+ * @param {string} password
+ * @param {string} name
+ * @param {number} age
+ * @returns
+ */
+export const register = (
+  email: string,
+  password: string,
+  name: string,
+  age: number
+) => {
+  // declare body to POST
+  let body = {
+    email: email,
+    name: name,
+    age: age,
+    password: password,
+  };
+
+  // send POST register request endpoint
+  // http://localhost:8000/api/auth/register
+  return axios.post("/auth/login", body);
+};
