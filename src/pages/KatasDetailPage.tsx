055566dc331ab076dd5f82448422845f03b0794e
Federico Rojas <federico.rojas.121@gmail.com>
2023-05-10 16:16:51 -0300

    update settings.json - added pages tsx files

---
+++ src/pages/KatasDetailPage.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const KatasDetailPage = () => {
+  return (
+    <div>
+      <h1>Katas Detail Page</h1>
+    </div>
+  );
+};
---
+++ src/pages/KatasPages.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const KatasPage = () => {
+  return (
+    <div>
+      <h1>Katas Page</h1>
+    </div>
+  );
+};
